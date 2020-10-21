6f93dc56e708d3e25b51353fb1c07b6d87ce417a
Jejoon Ryu <rjejoon@gmail.com>
2020-10-20 21:16:52 -0600

    added function initConnAndCurrFrom()

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sqlite3
-
+import os 
 
 def main():
     
+    conn, curr = initConnAndCurrFrom('schema.sql')
+
     run = True
     while run:
         
@@ -15,12 +17,29 @@ def main():
             signIn()
         elif uInput == 'su':
             signUp()
-
         else:
             print("error: command not found.")
 
+def initConnAndCurrFrom(f_name):
+
+    conn = sqlite3.connect('./socialmedia.db')
+    curr = conn.cursor()
+
+    path = os.path.abspath(os.path.dirname(__file__)) + os.sep + f_name
+    with open(path, 'r') as f:
+        
+        query = ''.join(f.readlines())
+       
+    curr.executescript(query)
+
+    conn.commit()
+
+    return conn, curr
 
 def signUp():
+    pass
+
+
 
 
 
