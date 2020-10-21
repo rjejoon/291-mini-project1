3add6e1eeb223636da7afd3e33afad9b50a960c1
Jejoon Ryu <rjejoon@gmail.com>
2020-10-20 23:12:27 -0600

    added sign up functionality

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import sqlite3
 import os 
+import getpass
+from datetime import date
 
 def main():
-    
-    conn, curr = initConnAndCurrFrom('schema.sql')
+    '''
+    The main loop of the program.
+    '''
+    conn, curr = initConnAndCurrFrom('schema.sql', 'socialmedia.db')
 
     run = True
     while run:
         
         # first screen
-        uInput = input("Options: (si) sign in, (su) sign up, (q)uit: ").lower()
+        uInput = input("\nOptions: (si) sign in, (su) sign up, (q)uit: ").lower()
 
         if uInput == 'q':
             run = False
         elif uInput == 'si':
             signIn()
         elif uInput == 'su':
-            signUp()
+            signUp(conn, curr)
         else:
             print("error: command not found.")
 
-def initConnAndCurrFrom(f_name):
+    conn.commit()
+    conn.close()
 
-    conn = sqlite3.connect('./socialmedia.db')
+
+def initConnAndCurrFrom(f_name, db_name):
+    '''
+    Reads in DDL from a text file and returns sqlite3 connection and cursor associated to the specified database.
+
+    Keyword arguments:
+    f_name —- text file containing SQL DDL
+    db_name -- .db file
+    '''
+    dir_path = os.path.abspath(os.path.dirname(__file__)) + os.sep
+    f_path = dir_path + f_name
+    db_path = dir_path + db_name 
+
+    conn = sqlite3.connect(db_path)
     curr = conn.cursor()
-
-    path = os.path.abspath(os.path.dirname(__file__)) + os.sep + f_name
-    with open(path, 'r') as f:
+    with open(f_path, 'r') as f:
         
         query = ''.join(f.readlines())
        
@@ -36,12 +52,88 @@ def initConnAndCurrFrom(f_name):
 
     return conn, curr
 
-def signUp():
-    pass
+
+def signUp(conn, curr):
+    '''
+    Prompts the user for an account information and saves in the database.
+
+    Keyword arguments:
+    conn -- sqlite3.Connection
+    curr -- sqlite3.Cursor
+    '''
+
+    valid = False
+    while not valid:
+
+        print()
+        uid = getID(conn, curr)
+        name = input("Enter your first and last name: ")
+        city = input("Enter your city: ")
+        pwd = getPassword()
+        crdate = str(date.today())
+            
+        print()
+        print("Please double check your information: ")
+        print("   id: {}".format(uid))
+        print("   name: {}".format(name))
+        print("   city: {}".format(city))
+
+        valid = checkValid()
+
+    print("Sign up successful!")
+
+    curr.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)", 
+            [uid, name, city, pwd, crdate])
+
+    conn.commit()
 
 
+def getID(conn, curr):
+    '''
+    Gets an appropriate user id and returns it.
+
+    Keyword arguments:
+    conn -- sqlite3.Connection
+    curr -- sqlite3.Cursor
+    '''
+    valid = False
+    while not valid: 
+        uid = input("Enter your id: ")
+        curr.execute("SELECT * FROM users WHERE uid = ?;", [uid])
+        if curr.fetchone():
+            print("error: user id already taken.")
+        else:
+            valid = True
+
+    return uid
 
 
+def getPassword():
+    '''
+    Gets an appropriate password and returns it.
+    '''
+    valid = False
+    while not valid:
+        pwd = getpass.getpass("Enter your password: ")
+        pwd2 = getpass.getpass("Enter the password again: ")
+        if pwd == pwd2:
+            valid = True
+        else:
+            print("error: passwords do not match.")
+
+    return pwd
+
+
+def checkValid():
+    '''
+    Prompts the user to double check their account information and returns the result.
+    '''
+    while True:
+        checkValid = input("\nIs this correct? y/n ").lower()
+        if checkValid == 'y':
+            return True
+        elif checkValid == 'n':
+            return False
 
 
 if __name__ == "__main__":
